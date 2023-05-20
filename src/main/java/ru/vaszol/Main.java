bd52ba3b9f2e18d428754aef6cbf36fb79a2c1a7
vasiliy <vaszol@mail.ru>
2023-05-20 04:48:09 +0300

    Добавлена сцена с кнопкой диалога выбора каталога

--- src/main/java/ru/vaszol/Main.java
+++ src/main/java/ru/vaszol/Main.java
@@ -1,16 +1,40 @@
 package ru.vaszol;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Map;
+
 public class Main extends Application {
+    private Stage stage;
+    private Map<String, Long> sizes;
+
     public static void main(String[] args) {
-//        System.out.println("Hello world!");
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        this.stage = stage;
+        stage.setTitle("Анализатор диска");
 
+        Button button = new Button("Выберите каталог");
+        button.setOnAction(actionEvent -> {
+            File directory = new DirectoryChooser().showDialog(stage);
+            String path = directory.getPath();
+            sizes = new Analizer().calculateDirectorySize(Path.of(path));
+            //..
+        });
+
+        StackPane pane = new StackPane();
+        pane.getChildren().add(button);
+        stage.setScene(new Scene(pane, 300, 250));
+        stage.show();
     }
 }
