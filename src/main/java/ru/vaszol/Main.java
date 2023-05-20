50bf16e94efaaae14e37560b187675be3e7b3864
vasiliy <vaszol@mail.ru>
2023-05-20 04:10:37 +0300

    first commit

---
+++ src/main/java/ru/vaszol/Main.java
@@ -0,0 +1,16 @@
+package ru.vaszol;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
